8e0b07b36db484718181b92e33c53f333366a59d
wtq <1906272972@qq.com>
2016-05-25 14:34:01 +0800

    modify calculate similarity add rna-similar-score.txt

--- script/issue2/issue2_similarity.py
+++ script/issue2/issue2_similarity.py
@@ -2,25 +2,18 @@
 # -*- coding: utf-8 -*-
 __author__ = 'wtq'
 
+import decimal
+import operator
 import traceback
 from mdat.db.conn_mongo import conn_mongo
 
 
-def combin(a, b):
-    m = a
-    n = b
-    if m < n:
-        m, n = n, m
-
-    fenmu = 1.0
-    fenzi = 1.0
-    for i in range(0, int(n)):
-        fenzi = fenzi * m
-        m = m-1
-    for i in range(0, int(n)):
-        fenmu = fenmu * (i + 1)
-
-    return float(fenzi / fenmu)
+def c1(a, b):
+    n = a
+    k = b
+    # 使用float则不能转化长整型的数，只能使用decimal
+    # 使用reduce来求组合，简化了代码
+    return decimal.Decimal(reduce(operator.mul, range(n - k + 1, n + 1)) / reduce(operator.mul, range(1, k + 1)))
 
 
 def similarity_element(n1, n2, c):
@@ -29,13 +22,17 @@ def similarity_element(n1, n2, c):
     :param n1: Number of times of n1
     :param n2: Number of times of n2
     :param c:  n1 and n2 the common times in second column
+    在用此公式计算时会出现问题，当n1=n2=c时，此时组合的下标出现了0，则此组合数为0，结果为1-0=1
+    当n1 n2 c 很大时，随着迭代此处的增加(c1(c, i)*c1(n1+n2-2*c, c-i))/c1(n1+n2-c, c)
+    会越来越小，最终趋于0，最后的结果就为1-1=0
     :return:
     """
     k = c
     try:
-        rate = 0
+        rate = decimal.Decimal(0)
         for i in range(1, k):
-            rate += ((combin(c, i)*combin(n1+n2-2*c, c-i))/combin(n1+n2-c, c))
+            rate += (c1(c, i)*c1(n1+n2-2*c, c-i))/c1(n1+n2-c, c)
+
         return 1 - rate
     except Exception, e:
         traceback.print_exc()
@@ -49,18 +46,21 @@ def calculate_similar():
     client = conn_mongo()
     db = client.md
     for item in db.rna_times.find():
-        n1 = item["rna1-times"]
-        n2 = item["rna2-times"]
-        c = item["common-times"]
-
-        similarity = similarity_element(n1, n2, c)
+        n1 = int(item["rna1-times"])
+        n2 = int(item["rna2-times"])
+        c = int(item["common-times"])
+        # 将decimal再转化为float，否则不能在mongo中存储
+        similarity = float(similarity_element(n1, n2, c))
         items = {
             "rna1": item["rna1"],
             "rna2": item["rna2"],
             "similar_sore": similarity
         }
-        db.similar_score.insert(items)
+        db.similar_score_new.insert(items)
 
 if __name__ == "__main__":
-    # print similarity_element(5.0, 6.0, 3)
+    # print similarity_element(4, 5, 3)
     calculate_similar()
+    # print c1(2, 2)
+
+
--- script/issue2/similarity_score_matrix.py
+++ script/issue2/similarity_score_matrix.py
@@ -14,28 +14,22 @@ def generate_matrix(file_path):
     generate the similar_score matrix then write it into csv
     :return:
     """
-    files = open(file_path, "w+")
+    files = file(file_path, "w+")
     rna1 = db.target_scan_split.distinct("item2")
-    list_length = []
     rna1.sort()
     rna2 = rna1
     try:
         for r1 in rna1:
             similar = []
             for r2 in rna2:
-                 if r1 == r2:
-                     similar.append(1)
-                 else:
-                     for i in db.similar_score.find({"$or": [{"rna1": r1, "rna2": r2}, {"rna1": r2, "rna2": r1}]}):
-                         similar.append(i['similar_sore'])
+                 for i in db.similar_score_new.find({"$or": [{"rna1": r1, "rna2": r2}, {"rna1": r2, "rna2": r1}]}):
+                     similar.append(i['similar_sore'])
 
-            list_length.append(len(similar))
             files.write(str(similar))
             files.write("\n")
-        print list_length
     except Exception:
         traceback.print_exc()
 
 if __name__ == "__main__":
-    generate_matrix("/home/wtq/rna-similar-sore-new.txt")
+    generate_matrix("/home/wtq/rna-similar-score.txt")
 
